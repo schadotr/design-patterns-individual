49c91cd4b0ff22726e5d548a57291327b916aa4d
Swapnil Chadotra <schadotr@asu.edu>
2022-10-19 13:39:44 -0700

    Updating Offering Menu class

--- DesignPattern/src/OfferingMenu.java
+++ DesignPattern/src/OfferingMenu.java
@@ -1,5 +1,15 @@
 package DesignPattern;
 
 public class OfferingMenu {
+    public void decideBidding(){
+        System.out.println("Decide Bidding ...."); // Decides the bidding
+    }
 
+    public void discussBidding() {
+        System.out.println("Discuss Bidding ...."); // Discusses the bidding
+    }
+
+    public void submitBidding() {
+        System.out.println("Submit Bidding ...."); // Submits the bidding
+    }
 }
