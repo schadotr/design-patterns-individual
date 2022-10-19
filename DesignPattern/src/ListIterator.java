7d32b1658bf35c3b08c389b48c3eb93468e798e6
Swapnil Chadotra <schadotr@asu.edu>
2022-10-19 13:34:08 -0700

    Cleaning the List Iterator Code

--- DesignPattern/src/ListIterator.java
+++ DesignPattern/src/ListIterator.java
@@ -2,12 +2,14 @@ package DesignPattern;
 
 import java.util.Iterator;
 
-public abstract class ListIterator {
-    public abstract boolean hasNext(Iterator<Product> iterator);
+public abstract class ListIterator { // Iterator for iterating the indexes of the list
+    public abstract boolean hasNext(Iterator<Product> iterator); // hasNext checks if the next object is null
 
-    public abstract Product next(Iterator<Product> iterator);
+    public abstract Product next(Iterator<Product> iterator); // Jumps to the next element in the list
 
-    public abstract void remove(Iterator<Product> iterator);
+    @SuppressWarnings("unused")
+    public abstract void remove(Iterator<Product> iterator); // Removes the object
 
-    public abstract void moveToHead(Iterator<Product> iterator);
+    @SuppressWarnings("unused")
+    public abstract void moveToHead(Iterator<Product> iterator); // Moves to the start of the list
 }
