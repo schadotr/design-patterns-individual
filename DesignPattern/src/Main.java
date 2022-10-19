fbbc4f1fb0f3539797ddb299f178fef0e3273659
Swapnil Chadotra <schadotr@asu.edu>
2022-10-19 13:35:04 -0700

    Adding Main File

---
+++ DesignPattern/src/Main.java
@@ -0,0 +1,8 @@
+package DesignPattern;
+
+public class Main {
+    public static void main(String[] args){
+        @SuppressWarnings(value = "unused")
+        Facade facadeObject = new Facade(); // Creating the facade object
+    }
+}
