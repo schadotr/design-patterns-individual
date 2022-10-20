69cba64f2fb90b0afefe73f6e25ff1fb1f124b68
Swapnil Chadotra <schadotr@asu.edu>
2022-10-19 21:01:44 -0700

    Adding Visitor Pattern

--- DesignPattern/src/Reminder.java
+++ DesignPattern/src/Reminder.java
@@ -1,5 +1,24 @@
 package DesignPattern;
 
 public class Reminder {
+    private final NodeVisitor nodeVisitor;
+    public Reminder(){
+        nodeVisitor = new ReminderVisitor(); // Implements the visitor pattern
+        System.out.println("**********************************************************");
+        System.out.println("                            Visitor Pattern                            ");
+        System.out.println("**********************************************************");
+    }
+    public void visitFacade(Facade facade) {
+        nodeVisitor.visitFacade(facade);
+    }
 
+    @SuppressWarnings("unused")
+    public void visitProduct(Product product) {
+        product.accept(nodeVisitor);
+    }
+
+    @SuppressWarnings("unused")
+    public void visitTrading(Trading trading) {
+        trading.accept(nodeVisitor);
+    }
 }
