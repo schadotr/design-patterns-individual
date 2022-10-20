d8434e2f24ab94d22ceb1370bb905465e2ea5829
Swapnil Chadotra <schadotr@asu.edu>
2022-10-19 20:55:20 -0700

    Added the Product

--- DesignPattern/src/Product.java
+++ DesignPattern/src/Product.java
@@ -1,13 +1,25 @@
 package DesignPattern;
 
 public class Product {
-    private String productName;
+    @SuppressWarnings(value="FieldMayBeFinal")
+    private String productName; // The name of the product i.e. Meat or Produce
+    @SuppressWarnings(value="FieldMayBeFinal")
+    private String value;
 
-    public Product(String productName){
+    public Product(String productName, String value){
         this.productName = productName;
+        this.value = value;
     }
 
     public String getProductName(){
         return this.productName;
+    }
+
+    public String getValue(){
+        return this.value;
+    }
+
+    public void accept(NodeVisitor nodeVisitor) {
+        nodeVisitor.visitProduct(this);
     }
 }
